e21bfad9ba52fb512761227cf39dc8266dd6ff82
ZoranKJava <38503197+ZoranKJava@users.noreply.github.com>
2019-06-06 15:56:53 +0200

    sql

---
+++ SQLQueryTRIGGERS.sql
@@ -0,0 +1,60 @@
+--Trigger Examples
+
+create table TrigEx
+(
+LastName varchar(50),
+FirstName varchar(50)
+)
+
+create trigger Notify
+on TrigEx
+for insert
+as
+print 'ATTENTION!!!'
+print 'A row was inserted into the TrigEx table...'
+SELECT 'Then inserrted data was: ', * from inserted
+SELECT * from TrigEx
+
+insert into TrigEx
+values
+('James', 'Jack'), ('James','Frank'),('Long','Mark')
+
+
+create trigger ModEx
+ON TrigEx
+for update 
+as
+print 'Attention!!!'
+print 'A row was modified in the TrigEx table...'
+
+select 'deleted: The original data was: ', *from deleted
+select 'inserted: The new data is: ', * from inserted
+select * from TrigEx
+
+update TrigEx
+set LastName='Longly'
+where FirstName='Mark' and LastName='Long'
+
+select * from TrigEx
+
+--Simple Transaction and Locking Example
+
+BEGIN TRANSACTION ML
+update Person.Person
+set LastName = 'Long'
+
+ROLLBACK TRANSACTION ML
+
+COMMIT TRANSACTION ML
+
+select * from Person.Person
+
+begin tran mtl
+update Person.Person
+set FirstName='Jack'
+where FirstName='Terri'  and LastName='Duffy'
+
+select * from Person.Person
+where BusinessEntityID=2
+
+
